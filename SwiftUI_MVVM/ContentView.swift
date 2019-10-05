a39078de5c9cb4ef4ea65ee8ba1253f481623a1d
Bill Song <bsong2009@gmail.com>
2019-10-05 11:17:58 -0500

    Initial Commit

---
+++ SwiftUI_MVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_MVVM
+//
+//  Created by William Song on 10/5/19.
+//  Copyright © 2019 awa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
